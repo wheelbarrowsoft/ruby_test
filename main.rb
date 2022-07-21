236ada733381ab201fbd335c92f06d67754a1b5b
dantevvp <dantevvp@gmail.com>
2022-07-21 18:00:11 -0300

    testing

---
+++ main.rb
@@ -0,0 +1,15 @@
+class Main
+  def initialize
+    @name = 'Ruby test'    
+  end
+
+  def start
+    puts "Starting #{@name}"
+  end
+end
+
+puts 'a'
+
+main = Main.new
+
+main.startputs 'added by me'
