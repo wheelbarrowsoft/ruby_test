d5b37c673ac1719a4d64e758205e47c22721faff
dantevvp <dantevvp@gmail.com>
2022-08-29 15:09:54 -0300

    add more specs

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,9 +2,14 @@ require "main"
 
 RSpec.describe Main do
   describe "#greet" do
-    it "says your correct name" do
+    it "says hola dante" do
       result = described_class.new("dante").greet
       expect(result).to eq("hola dante")
+    end
+
+    it "says hola carlos" do
+      result = described_class.new("carlos").greet
+      expect(result).to eq("hola carlos")
     end
 
     it "doesn't like short names" do
