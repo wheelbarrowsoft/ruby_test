e10d0b6a587a907bd071c59ebec9f93afced3667
Dante Ventieri <dantevvp@gmail.com>
2023-06-06 13:45:48 -0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -19,3 +19,17 @@ puts 'added by me'
 puts 'added by me'
 puts 'added by me'
 puts 'added by me'
+
+class Example
+  def perform(id)
+    if self
+      Model.find_by!(id:, something: :else)
+    else
+      if self
+        Model.find_by!(id:, something: :else)
+      else
+        Model.find_by!(id:, something: :else)
+      end
+    end
+  end
+end
